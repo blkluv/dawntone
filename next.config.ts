55104d7b7ec5f95dfe9532143fce1557db76420c
Sato Yuta <112250397+310-whtwht@users.noreply.github.com>
2025-06-15 06:03:00 +0900

    Configure Monaco assets for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  webpack(config) {
+    config.resolve.alias["vs"] = path.join(__dirname, "public/vs");
+    return config;
+  },
 };
 
 export default nextConfig;
